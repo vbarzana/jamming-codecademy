32c2719f624e4867b3e73d5ca724b4de8f65fa8c
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2020-09-10 09:29:58 +0000

    Updates

---
+++ precache-manifest.5a1cdd5f9773c42fec1a67545bcf2416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jamming-codecademy/static/media/background_photo_desktop.37be7357.jpg"
+  },
+  {
+    "revision": "c4e84f9b79f5556109d6",
+    "url": "/jamming-codecademy/static/js/runtime~main.c4e84f9b.js"
+  },
+  {
+    "revision": "00be096aee5b0c78d543",
+    "url": "/jamming-codecademy/static/js/main.00be096a.chunk.js"
+  },
+  {
+    "revision": "84f7595243cbb9e63621",
+    "url": "/jamming-codecademy/static/js/1.84f75952.chunk.js"
+  },
+  {
+    "revision": "00be096aee5b0c78d543",
+    "url": "/jamming-codecademy/static/css/main.04fc1319.chunk.css"
+  },
+  {
+    "revision": "04e8d2c313a1edac5210d75f9f2e9f6d",
+    "url": "/jamming-codecademy/index.html"
+  }
+];
